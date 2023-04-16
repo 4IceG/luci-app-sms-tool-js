afbb5ceff76ed7e9d3bd12eb1f5f624c8151547e
Rafał Wabik <4rafal@gmail.com>
2023-04-16 13:49:23 +0200

    Fix "Permission denied" error.

--- luci-app-sms-tool-js/root/etc/uci-defaults/setup_sms_tool_js.sh
+++ luci-app-sms-tool-js/root/etc/uci-defaults/setup_sms_tool_js.sh
@@ -5,6 +5,10 @@
 chmod +x /sbin/sms_led.sh 2>&1 &
 chmod +x /sbin/smstool_led.sh 2>&1 &
 chmod +x /sbin/new_cron_sync.sh 2>&1 &
+chmod +x /etc/uci-defaults/off_sms.sh 2>&1 &
+chmod +x /etc/uci-defaults/setup_sms_tool_js.sh 2>&1 &
+chmod +x /etc/init.d/my_new_sms 2>&1 &
+
 rm -rf /tmp/luci-indexcache  2>&1 &
 rm -rf /tmp/luci-modulecache/  2>&1 &
 exit 0
