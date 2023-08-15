e2b963be8f47488b4717389e03dfe4a12faa0418
Rafał Wabik <4rafal@gmail.com>
2023-08-15 14:01:47 +0200

    Upgrade to 2.0.12-20230815

--- luci-app-sms-tool-js/htdocs/luci-static/resources/view/modem/readsms.js
+++ luci-app-sms-tool-js/htdocs/luci-static/resources/view/modem/readsms.js
@@ -366,8 +366,8 @@ return view.extend({
 									/* No merging messages */
 									if (smsM == "0") {
 									
-										/* Sort messages by delivery time */
-										var sortbyTime = json.sort((function (a, b) { return new Date(b.timestamp) - new Date(a.timestamp) }));
+									/* Sorting messages by delivery time */
+									var sortbyTime = json.sort((function (a, b) { return new Date(b.timestamp) - new Date(a.timestamp) }));
 
 										/* Sorting messages by parts */
 										var sortedData = sortbyTime.sort((a, b) => {
@@ -375,8 +375,8 @@ return view.extend({
         											return a.part - b.part;
     										} else {
         											return 0;
-    										}
-									});
+    											}
+										});
 
 										if (u){
 
